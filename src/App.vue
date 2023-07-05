d7ae12d09f2647aed730660d4b9e9e18a743e945
Deltafortal <aldolucchetta21@gmail.com>
2023-07-05 11:57:12 +0200

    Boilerplate

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<!---------------- Script ----------------->
+
+<script>
+
+</script>
+
+
+
+
+
+
+
+
+<!--------------- Template ------------->
+
+<template>
+  
+</template>
+
+
+
+
+
+
+
+<!---------------Style -------------->
+
+<style lang="scss">
+
+@use './assets/scss/style.scss'
+
+</style>
