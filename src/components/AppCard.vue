d87c6455a08539b7b193e09521ccd581851ff3ce
Deltafortal <aldolucchetta21@gmail.com>
2023-07-06 12:55:34 +0200

    Added flag

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -9,8 +9,18 @@ export default {
         item: Object
     },
 
+    computed: {
+        hasFlag() {
+            const flags = ['it', 'en'];
+            return flags.includes(this.item.original_language)
+        },
 
-    
+        imgUrl() {
+            const url = new URL (`../assets/img/${this.item.original_language}.png`, import.meta.url);
+            return url.href
+        }
+    }
+
 };
 
 </script>
@@ -28,7 +38,10 @@ export default {
   
     <h2>{{ item.title || item.name }}</h2>
     <h4> {{ item.original_title || item.original_name }}</h4>
-    <div> {{ item.original_language }}</div>
+    <div> 
+        <img v-if="hasFlag" :src="imgUrl" :alt="item.original_language">
+        <div v-else>{{ item.original_language }}</div>
+    </div>
     <div> {{ item.vote_average }}</div>
 
 
